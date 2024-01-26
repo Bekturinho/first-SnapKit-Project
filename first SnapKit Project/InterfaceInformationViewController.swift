1ddd0d1f335bc7d65ebaf1da92c6cf9780781b40
bekturinho <ismanalievbektur4@gmail.com>
2024-01-25 21:53:43 -0800

    sdsd

---
+++ first SnapKit Project/InterfaceInformationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  InterfaceInformationViewController.swift
+//  first SnapKit Project
+//
+//  Created by fortune cookie on 1/25/24.
+//
+
+import UIKit
+
+class InterfaceInformationViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
