b3be149bef1a76e30b60e37ccbc6e4bc66b9c60e
bekturinho <ismanalievbektur4@gmail.com>
2024-01-25 17:22:41 -0800

    Initial Commit

---
+++ first SnapKit Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first SnapKit Project
+//
+//  Created by fortune cookie on 1/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
